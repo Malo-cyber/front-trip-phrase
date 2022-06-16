5e63aff826437f81520e6ecf459b726f5b81d8b4
TOQUET Armand <armand-externe.toquet@edf.fr>
2022-06-16 18:39:55 +0200

    feat : add HYBRID

---
+++ android/app/src/main/java/com/malotoq/phrasetrip/MainActivity.java
@@ -0,0 +1,5 @@
+package com.malotoq.phrasetrip;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.malotoq.phrasetrip',
+  appName: 'front-trip-phrase',
+  webDir: 'dist/front-trip-phrase',
+  bundledWebRuntime: false
+};
+
+export default config;
